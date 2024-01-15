93464bb60e6b0dd07a8799a84a86444d8e9b82dd
t.mesropyan <tatul.mesropyan-extern@invia.de>
2024-01-15 13:43:15 +0400

    S3 public access policy adjustments

--- s3.tf
+++ s3.tf
@@ -1,6 +1,28 @@
 resource "aws_s3_bucket" "frontend" {
   bucket        = "${local.resource_prefix}-frontend"
   force_destroy = false
+  tags = {
+    Name = "ama-task-frontend"
+  }
+}
+resource "aws_s3_bucket_cors_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+  }
+}
+resource "aws_s3_bucket_website_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
 }
 resource "aws_s3_bucket_cors_configuration" "frontend" {
   bucket = aws_s3_bucket.frontend.id
@@ -18,10 +40,6 @@ resource "aws_s3_bucket_public_access_block" "frontend" {
   block_public_acls       = true
   block_public_policy     = false
   ignore_public_acls      = true
-}
-
-resource "aws_cloudfront_origin_access_identity" "frontend" {
-  comment = "${local.resource_prefix}-frontend_origin_access_identity"
 }
 resource "aws_s3_bucket_policy" "frontend" {
   bucket = aws_s3_bucket.frontend.id
